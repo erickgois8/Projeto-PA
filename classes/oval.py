a0b580f748d4164d2567a5b1385fde9779b6b828
arthursantos765 <jorge.dossantos@dcomp.ufs.br>
2026-07-02 23:26:06 -0300

    adição de classes oval e quadrado

--- classes/oval.py
+++ classes/oval.py
@@ -1,4 +1,17 @@
 from figura import Figura
+from dataclasses import dataclass, field
 
 class Oval(Figura):
-    pass
+    cor_de_borda: str
+    cor_de_preenchimento: str
+    xi: int
+    yi: int
+    xf: int
+    yf: int
+
+    def desenhar(self, canvas, dash=()):
+        return canvas.create_oval(self.xi, self.yi, self.xy, self.yf,
+                                   fill = self.cor_de_preenchimento, outline = self.cor_de_borda, dash = dash)
+    
+    def incompleta(self):
+        return (self.xi, self.yi) == (self.xf, self.yf)
--- classes/quadrado.py
+++ classes/quadrado.py
@@ -2,4 +2,20 @@ from figura import Figura
 from dataclasses import dataclass, field
 
 class Quadrado(Figura):
-    pass
+    cor_de_borda: str 
+    cor_de_preenchimento: str
+    xi: int
+    yi: int
+    xf: int
+    yf: int
+
+    @property
+    
+    def pontos(self):
+        return (self.xi, self.yf, self.xf, self.yf)
+
+    def desenhar (self, canvas, dash=()):
+        canvas.create_rectangle(self.pontos, fill = self.cor_preenchimento, outline = self.cor_borda, dash = dash)
+
+    def incompleta(self):
+        return (self.xi, self.yi) == (self.xf, self.yf)

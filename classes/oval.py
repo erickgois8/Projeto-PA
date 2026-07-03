38a9c8fdbe699eabd02ec505b78cebf002dba24a
Erick Gois <erickgois837@gmail.com>
2026-07-02 23:39:59 -0300

    mudanças pontuais nas classes oval e quadrado

--- classes/oval.py
+++ classes/oval.py
@@ -1,17 +1,18 @@
 from figura import Figura
 from dataclasses import dataclass, field
 
+@dataclass
 class Oval(Figura):
-    cor_de_borda: str
-    cor_de_preenchimento: str
     xi: int
     yi: int
     xf: int
     yf: int
+    cor_borda: str
+    cor_preenchimento: str
 
     def desenhar(self, canvas, dash=()):
-        return canvas.create_oval(self.xi, self.yi, self.xy, self.yf,
-                                   fill = self.cor_de_preenchimento, outline = self.cor_de_borda, dash = dash)
+        canvas.create_oval(self.xi, self.yi, self.xf, self.yf,
+                                   fill=self.cor_preenchimento, outline=self.cor_de_borda, dash=dash)
     
     def incompleta(self):
         return (self.xi, self.yi) == (self.xf, self.yf)
--- classes/quadrado.py
+++ classes/quadrado.py
@@ -1,21 +1,21 @@
 from figura import Figura
 from dataclasses import dataclass, field
 
+@dataclass
 class Quadrado(Figura):
-    cor_de_borda: str 
-    cor_de_preenchimento: str
     xi: int
     yi: int
     xf: int
     yf: int
+    cor_borda: str 
+    cor_preenchimento: str
 
     @property
-    
     def pontos(self):
-        return (self.xi, self.yf, self.xf, self.yf)
+        return (self.xi, self.yi, self.xf, self.yf)
 
-    def desenhar (self, canvas, dash=()):
-        canvas.create_rectangle(self.pontos, fill = self.cor_preenchimento, outline = self.cor_borda, dash = dash)
+    def desenhar(self, canvas, dash=()):
+        canvas.create_rectangle(self.pontos, fill=self.cor_preenchimento, outline = self.cor_borda, dash=dash)
 
     def incompleta(self):
         return (self.xi, self.yi) == (self.xf, self.yf)

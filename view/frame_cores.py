c49c746682c51bd2285234686e537b3a47380d99
arthursantos765 <jorge.dossantos@dcomp.ufs.br>
2026-07-06 22:51:09 -0300

    alteração em frame_cores

--- view/frame_cores.py
+++ view/frame_cores.py
@@ -2,4 +2,5 @@ from tkinter import *
 
 class FrameCores:
     def __init__(self, root):
-        self.frame = Frame(root, bg="#000000", padx= 20, pady=20)
+        self.frame = Frame(root, bg="#000000", width=100, height=137)
+        self.frame.pack(padx=10, pady=600)

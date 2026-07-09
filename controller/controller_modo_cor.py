5f9c1bd1c8c223903c5d248c1663c7a24b392718
s2Sas <444sophiasilva@gmail.com>
2026-07-09 17:38:04 -0300

    atualizando controllers

--- controller/controller_modo_cor.py
+++ controller/controller_modo_cor.py
@@ -2,4 +2,17 @@ from model.cores import Cores
 from view.janela import Janela
 
 class ControllerModoCor:
-    pass
+    def __init__(self, view: Janela, cores: Cores):
+        botoes_atalhos = view.frame_lateral.frame_atalhos.botoes_atalhos
+
+        # Botão cor da borda
+        botoes_atalhos.btn_cor_borda.config(
+            command=lambda: cores.selecionar_modo("borda"))
+
+        # Botão figura preenchida
+        botoes_atalhos.btn_fig_preenchida.config(
+            command=lambda: cores.selecionar_modo("preenchimento"))
+
+        # Botão retira preenchimento
+        botoes_atalhos.btn_figura_vazia.config(
+            command=lambda: cores.selecionar_modo("preenchimento"))

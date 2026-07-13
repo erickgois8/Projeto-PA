a75056f74f4576c0ed6428dac02c6d52503a53dd
s2Sas <444sophiasilva@gmail.com>
2026-07-13 14:49:12 -0300

    adicionando as funcionalidades de abrir e salvar

--- controller/controller_arquivos.py
+++ controller/controller_arquivos.py
@@ -3,6 +3,6 @@ from controller.controller_principal import ControllerPrincipal
 
 class ControllerArquivos:
     def __init__(self, view: Janela, controller: ControllerPrincipal):
-        view.botoes_salvar.botao_salvar.configure(command=controller.arquivos.salvar)
+        view.botoes_salvar.botao_abrir.configure(command=lambda: controller.arquivos.abrir())
         
-        view.botoes_salvar.botao_abrir.configure(command=controller.arquivos.abrir)
+        view.botoes_salvar.botao_salvar.configure(command=controller.arquivos.salvar)

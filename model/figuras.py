6069ba376d363ce2081e91a43b306828ecd914b4
arthursantos765 <jorge.dossantos@dcomp.ufs.br>
2026-07-16 22:33:41 -0300

    alterações pontuais

--- model/figuras.py
+++ model/figuras.py
@@ -4,41 +4,41 @@ from dataclasses import dataclass, field
 # Gerencia o armazenamento das figuras
 @dataclass
 class Figuras:
-    __dados: list[Figura] = field(default_factory=list)
+        __dados: list[Figura] = field(default_factory=list)
 
-    def acessar(self):
-        return self.__dados
-    
-    def adicionar(self, figura):
-        self.__dados.append(figura)
+        def acessar(self):
+            return self.__dados
+        
+        def adicionar(self, figura):
+            self.__dados.append(figura)
 
-    def remover(self, figura):
-        self.__dados.remove(figura)
+        def remover(self, figura):
+            self.__dados.remove(figura)
 
-    def mover_para_frente(self, figura_selecionada):
-        if figura_selecionada is not None:
-            indice = self.__dados.index(figura_selecionada)
+        def mover_para_frente(self, figura_selecionada):
+            if figura_selecionada in self.__dados:
+                indice = self.__dados.index(figura_selecionada)
 
-            if indice < len(self.__dados) - 1:
-                self.__dados[indice], self.__dados[indice + 1] = self.__dados[indice + 1], self.__dados[indice]
-            
-    def mover_para_tras(self, figura_selecionada):
-        if figura_selecionada is not None:
-            indice = self.__dados.index(figura_selecionada)
+                if indice < len(self.__dados) - 1:
+                    self.__dados[indice], self.__dados[indice + 1] = self.__dados[indice + 1], self.__dados[indice]
+                
+        def mover_para_tras(self, figura_selecionada):
+            if figura_selecionada in self.__dados:
+                indice = self.__dados.index(figura_selecionada)
 
-            if indice > 0:
-                self.__dados[indice], self.__dados[indice - 1] = self.__dados[indice - 1], self.__dados[indice]
+                if indice > 0:
+                    self.__dados[indice], self.__dados[indice - 1] = self.__dados[indice - 1], self.__dados[indice]
 
-    def mover_para_topo(self, figura_selecionada):
-        if figura_selecionada is not None:
-            indice = self.__dados.index(figura_selecionada)
+        def mover_para_topo(self, figura_selecionada):
+            if figura_selecionada in self.__dados:
+                indice = self.__dados.index(figura_selecionada)
 
-            figura_excluida = self.__dados.pop(indice)
-            self.__dados.append(figura_excluida)
+                figura_excluida = self.__dados.pop(indice)
+                self.__dados.append(figura_excluida)
 
-    def mover_para_fundo(self, figura_seleciondada):
-        if figura_seleciondada is not None:
-            indice = self.__dados.index(figura_seleciondada)
+        def mover_para_fundo(self, figura_seleciondada):
+            if figura_seleciondada in self.__dados:
+                indice = self.__dados.index(figura_seleciondada)
 
-            figura_excluida = self.__dados.pop(indice)
-            self.__dados.insert(0, figura_excluida)
+                figura_excluida = self.__dados.pop(indice)
+                self.__dados.insert(0, figura_excluida)
